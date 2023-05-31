da97b62f9a001003dda219b38e5b9f8c1dd21d32
LayerDE <the_gamer@gmx.net>
2023-05-31 14:30:30 +0200

    rename way -> steps

--- Inc/bldc.h
+++ Inc/bldc.h
@@ -7,7 +7,7 @@ extern volatile int pwmr;               // global variable for pwm right. -1000 
 extern uint8_t enable;                  // global variable for motor enable
 
 extern int16_t batVoltage;              // global variable for battery voltage
-extern volatile uint16_t way[2]
+extern volatile uint16_t steps[2]
 extern volatile uint32_t buzzerTimer;
 extern uint8_t buzzerCount;             // global variable for the buzzer counts. can be 1, 2, 3, 4, 5, 6, 7...
 extern uint8_t buzzerFreq;              // global variable for the buzzer pitch. can be 1, 2, 3, 4, 5, 6, 7...
